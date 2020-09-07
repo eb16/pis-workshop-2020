e5f016db2d73a85c2c1c128ea3d8e1a08a38168a
Emiliano Botti <emiliano@xmartlabs.com>
2020-09-07 13:12:20 -0300

    Adds todo card and todo items with minimum styles

--- react/pis-workshop-2020-react/src/pages/home/home.jsx
+++ react/pis-workshop-2020-react/src/pages/home/home.jsx
@@ -4,9 +4,20 @@ import styles from './home.module.scss';
 
 const Home = () => (
   <div className={styles.container}>
+
     <h1 className={styles.title}>
-      Welcome! This is the homepage.
+      Todo list for Tuesday morning
     </h1>
+    <div className={styles.todoCard}>
+      <div className={styles.todoItem}>
+        <input type="checkbox" name="apples" />
+        <label htmlFor="apples">Buy x6 Apples</label>
+      </div>
+      <div className={styles.todoItem}>
+        <input type="checkbox" name="pears" />
+        <label htmlFor="pears">Buy x8 Pears</label>
+      </div>
+    </div>
   </div>
 );
 

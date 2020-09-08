9ce85bf2ec83a67fbc809ae9781bd419b2f0fc74
Matías Lorenzo <mlorenzo@xmartlabs.com>
2020-09-08 10:49:00 -0300

    adds interaction between backend and frontend

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Product.count == 0
+  product_data = [
+    {
+      name: 'Heladera',
+      description: 'Enfría cosas',
+      price: 45_234
+    },
+    {
+      name: 'Televisión',
+      description: 'Es como una ventana a mil realidades diferentes, pero nada que ver.',
+      price: 20_324
+    },
+    {
+      name: 'Horno',
+      description: 'Calienta cosas',
+      price: 1000
+    },
+    {
+      name: 'Freidora sin aceinte',
+      description: 'Calienta cosas, pero sin aceite',
+      price: 5000
+    },
+    {
+      name: 'Equipo de audio',
+      description: 'Emite sonidos',
+      price: 4000
+    },
+  ]
+
+  product_data.each { |data| Product.create(data) }
+end
